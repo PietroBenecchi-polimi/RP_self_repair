b4b77b9d3c6f15cad3e5438770ef3b4b8aa825d0
pietrobenecchi <pietro.benecchi@gmail.com>
2025-03-31 12:36:11 +0200

    Add SMONG and Random oversampling

--- Oversampling.py
+++ Oversampling.py
@@ -3,9 +3,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ast
 from LIME import explain_prediction_with_lime 
-import re
 import smogn
 
+# Synthetic Minority Over-Sampling Technique for Regression with Gaussian Noise 
+#https://github.com/nickkunz/smogn?tab=readme-ov-file
+def smote_oversampling(X):
+    df = X.copy()
+    df["opt_config"] = df["opt_config"].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+    df = df["opt_config"].apply(pd.Series)
+    
+    df_resampled = smogn.smoter(data=df, y='SCS')
+         
+    return df_resampled
+
+def random_oversampling(X, n_samples):
+    synthetic_data = {}
+    
+    df = X.copy()
+    df["opt_config"] = df["opt_config"].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+    df = df["opt_config"].apply(pd.Series)
+    
+    for column in df.columns:
+        col_min, col_max = df[column].min(), df[column].max()
+        synthetic_data[column] = np.random.uniform(col_min, col_max, n_samples)
+    
+    return pd.DataFrame(synthetic_data)
 
 def lime_based_resampling(X, explanations, instance_to_oversampling, scale_factor=1.0):
     """
@@ -55,10 +77,14 @@ def main():
 
     # new samples based on LIME explanations
     resampled_X = lime_based_resampling(X, explanations_df, X.shape[0], scale_factor=0.1)
-    
+    resampled_X_smogn = smote_oversampling(X)
+    random_oversampling_X = random_oversampling(X, X.shape[0])
 
     output_path = "lime_resampled_data.csv"
+
     resampled_X.to_csv(output_path, index=False)
+    resampled_X_smogn.to_csv("smote_resampled_data.csv", index=False)
+    random_oversampling_X.to_csv("random_oversampling.csv", index=False)
     
 if __name__ == "__main__":
     main()
