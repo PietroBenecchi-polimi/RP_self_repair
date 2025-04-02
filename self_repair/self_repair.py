4c2608de8e8d01303c20b0f6845441c3832e61cf
pietrobenecchi <pietro.benecchi@gmail.com>
2025-04-02 15:20:25 +0200

    Add verification after training

--- self_repair/self_repair.py
+++ self_repair/self_repair.py
@@ -21,8 +21,9 @@ def validate_configurations(opt_config, ground_truth, FTG_threshold=0.01):
         opt_SCS = opt_config.iloc[i]['SCS']
         mc_SCS = ground_truth.iloc[i]['SCS']
         validity = validate_scs(opt_SCS, mc_SCS)
+        validity_array.append(validity)
 
-        # Validate FTG parameter
+       # Validate FTG parameter
 #      mc_ftg = ground_truth["FTG"] 
 #      opt_ftg = opt_config.iloc[i]['FTG']
 #      validity = validity and validate_ftg(mc_ftg, opt_ftg, FTG_threshold)
@@ -31,16 +32,16 @@ def validate_configurations(opt_config, ground_truth, FTG_threshold=0.01):
             data_list.append(opt_config.iloc[i])
 
     # Calculate validation metrics
-#    invalid_count = sum(not v for v in validity_array)
-#    if(len(validity_array) == 0):
-#        success_percentage = 0
-#    else:
-#        success_percentage = round(1 - (invalid_count / len(validity_array)), 2)
+    invalid_count = sum(not v for v in validity_array)
+
+    if(len(validity_array) == 0):
+        success_percentage = 0
+    else:
+        success_percentage = round(1 - (invalid_count / len(validity_array)), 2)
 
     df = pd.DataFrame(data_list)
-    df.to_csv("invalid_config.csv", index=False)
-    print(df)
-    return df
+    
+    return df, success_percentage
 
 
 def upload_samples_and_regressor(ground_truth=None, regressor=None, samples=None, SCS_threshold=0.01):
@@ -58,10 +59,21 @@ def upload_samples_and_regressor(ground_truth=None, regressor=None, samples=None
     ground_truth = ground_truth.predict(sampled_df)
     ground_truth = pd.DataFrame(ground_truth, columns=['SCS'])
 
-    invalid_config = validate_configurations(sampled_df, ground_truth, FTG_threshold=0.01)
+    invalid_config, _ = validate_configurations(sampled_df, ground_truth, FTG_threshold=0.01)
     invalid_config = pd.DataFrame(invalid_config)
 
     return invalid_config, regressor
+
+def validate_configurations_after_retraining(regressor, ground_truth):
+    ground_truth = joblib.load(ground_truth)
+    data = pd.read_csv("datasets/transformed_dataset.csv")
+    new_data = data.drop(columns=["SCS", "FTG"])
+
+    ground_truth_data = ground_truth.predict(new_data)
+    ground_truth_data = pd.DataFrame(ground_truth_data, columns=['SCS'])
+    new_data['SCS'] = regressor.predict(new_data)
+    
+    return validate_configurations(data, ground_truth_data, FTG_threshold=0.01)
 
 def oversampling_methods(invalid_config, n_samples=100):
     new_samples_list = []
@@ -101,12 +113,21 @@ def main():
     # Oversampling methods
     new_samples_list = oversampling_methods(invalid_config, n_samples=100)
 
+    stats = []
     #Retrain regressor with oversampled data
     for new_samples in new_samples_list:
-        regressor.fit(new_samples.get("samples").drop(columns=["SCS"]), new_samples.get("samples")["SCS"])
+        X = new_samples.get("samples").drop(columns=["SCS"])
+        y = new_samples.get("samples")["SCS"]
+        regressor.fit(X, y)
         # Validate results using new and unseen data in training
-        #validate_configurations(new_samples.get("samples"), ground_truth, FTG_threshold=0.01)
-
+        df, success_percentage = validate_configurations_after_retraining(regressor, ground_truth)
+        stats.append({
+           "method": new_samples.get("method"),
+           "success_percentage": success_percentage
+        })
+    
+    print(stats)
+    print("Best resampling method is: ", max(stats, key=lambda x: x['success_percentage']), "YUPPY!")
     # Restore function, something like early stopping so that I can take the best model
 
 if __name__ == "__main__":
