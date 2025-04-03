c8c486bfabf5dda69ed9367bbd21c9af4344fe6d
Luigi Cardone <lcardone22@gmail.com>
2025-04-03 18:27:40 +0200

    validtate_scs now considers ub and lb, added logger

--- self_repair/self_repair.py
+++ self_repair/self_repair.py
@@ -2,48 +2,11 @@ import pandas as pd
 import joblib
 from oversampling import random_oversampling, lime_based_resampling, smote_oversampling
 from validation import validate_configurations
+import sys
+import os
 
-def validate_configurations(opt_config, ground_truth, FTG_threshold=0.01):
-    validity_array = []
-    def validate_scs(opt_SCS, mc_SCS):
-        if opt_SCS > 0.9:
-            return bool(mc_SCS)
-        else:
-            return not bool(mc_SCS)
-
-#    def validate_ftg(mc_ftg, opt_ftg, threshold=FTG_threshold):
-#        if mc_ftg == 0 and opt_ftg == 0:
-#            return True
-#        else:
-#            return abs(mc_ftg - opt_ftg) <= threshold
-    data_list = []
-    # Iterate through the results and validate each configuration
-    for i in range(len(opt_config)):
-        opt_SCS = opt_config.iloc[i]['SCS']
-        mc_SCS = ground_truth.iloc[i]['SCS']
-        validity = validate_scs(opt_SCS, mc_SCS)
-        validity_array.append(validity)
-
-       # Validate FTG parameter
-#      mc_ftg = ground_truth["FTG"] 
-#      opt_ftg = opt_config.iloc[i]['FTG']
-#      validity = validity and validate_ftg(mc_ftg, opt_ftg, FTG_threshold)
-
-        if(not validity):
-            data_list.append(opt_config.iloc[i])
-
-    # Calculate validation metrics
-    invalid_count = sum(not v for v in validity_array)
-
-    if(len(validity_array) == 0):
-        success_percentage = 0
-    else:
-        success_percentage = round(1 - (invalid_count / len(validity_array)), 2)
-
-    df = pd.DataFrame(data_list)
-    
-    return df, success_percentage
-
+sys.path.append(os.path.abspath("utils"))
+from rp_logger import logger
 
 def upload_samples_and_regressor(ground_truth=None, regressor=None, samples=None, SCS_threshold=0.01):
     # Upload models
@@ -127,8 +90,8 @@ def main():
            "success_percentage": success_percentage
         })
     
-    print(stats)
-    print("Best resampling method is: ", max(stats, key=lambda x: x['success_percentage']), "YUPPY!")
+    logger.log(stats)
+    logger.log("Best resampling method is: ", max(stats, key=lambda x: x['success_percentage']), "YUPPY!")
     # Restore function, something like early stopping so that I can take the best model
 
 if __name__ == "__main__":
--- self_repair/validation.py
+++ self_repair/validation.py
@@ -1,33 +1,47 @@
 import pandas as pd
+import sys
+import os
 
-def validate_configurations(opt_config, ground_truth, FTG_threshold=0.01):
-    validity_array = []
-    def validate_scs(opt_SCS, mc_SCS):
+sys.path.append(os.path.abspath("utils"))
+from rp_logger import logger
+
+def validate_scs(opt_SCS, mc_SCS, mc_ub = 1, mc_lb = 0):
         if opt_SCS > 0.9:
-            return bool(mc_SCS)
+            return bool(mc_SCS) and mc_SCS <= mc_ub
         else:
-            return not bool(mc_SCS)
+            return not bool(mc_SCS) and mc_SCS >= mc_lb
 
-#    def validate_ftg(mc_ftg, opt_ftg, threshold=FTG_threshold):
-#        if mc_ftg == 0 and opt_ftg == 0:
-#            return True
-#        else:
-#            return abs(mc_ftg - opt_ftg) <= threshold
-    data_list = []
+def validate_ftg(mc_ftg, opt_ftg, threshold=FTG_threshold):
+    if mc_ftg == 0 and opt_ftg == 0:
+        return True
+    else:
+        return abs(mc_ftg - opt_ftg) <= threshold
+
+def validate_configurations(opt_results, ground_truth, FTG_threshold=0.005):
+    validity_array = []
+    
+    invalid_results = []
     # Iterate through the results and validate each configuration
-    for i in range(len(opt_config)):
-        opt_SCS = opt_config.iloc[i]['SCS']
+    for i in range(len(opt_results)):
+        opt_SCS = opt_results.iloc[i]['SCS']
         mc_SCS = ground_truth.iloc[i]['SCS']
-        validity = validate_scs(opt_SCS, mc_SCS)
+        mc_SCS_ub = ground_truth.iloc[i]['PRSCS_UB']
+        mc_SCS_lb = ground_truth.iloc[i]['PRSCS_LB']
+        if not pd.isna(mc_SCS_ub) and not pd.isna(mc_SCS_lb):
+            logger.debug("UB and LB validity will be assesed for SCS metric")
+            validity = validate_scs(opt_SCS, mc_SCS, mc_SCS_ub, mc_SCS_lb)
+        else:
+            logger.warning("Only binary validity will be assesed for SCS metric")
+            validity = validate_scs(opt_SCS, mc_SCS)
         validity_array.append(validity)
 
        # Validate FTG parameter
-#      mc_ftg = ground_truth["FTG"] 
-#      opt_ftg = opt_config.iloc[i]['FTG']
-#      validity = validity and validate_ftg(mc_ftg, opt_ftg, FTG_threshold)
+        mc_ftg = ground_truth["FTG"]
+        opt_ftg = opt_results.iloc[i]['FTG']
+        validity = validity and validate_ftg(mc_ftg, opt_ftg, FTG_threshold)
 
         if(not validity):
-            data_list.append(opt_config.iloc[i])
+            invalid_results.append(opt_results.iloc[i])
 
     # Calculate validation metrics
     invalid_count = sum(not v for v in validity_array)
@@ -37,6 +51,6 @@ def validate_configurations(opt_config, ground_truth, FTG_threshold=0.01):
     else:
         success_percentage = round(1 - (invalid_count / len(validity_array)), 2)
 
-    df = pd.DataFrame(data_list)
+    invalid_results_df = pd.DataFrame(invalid_results)
     
-    return df, success_percentage
+    return invalid_results_df, success_percentage
